23629d2b77bd08232a6faef53ead0769fe795a01
nou <nou13n8e@gmail.com>
2023-06-26 13:53:21 +0900

    IndexController.java에 모델 객체 생성 sayHello 변수 추가

--- src/main/java/nou/hello/spring4/controller/IndexController.java
+++ src/main/java/nou/hello/spring4/controller/IndexController.java
@@ -1,13 +1,18 @@
 package nou.hello.spring4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
     // http://localhost:8080/ 호출시 실행
     @RequestMapping("/")
-    public String index() {
+    public String index(Model m) {
+
+        // addAttribute(변수명, 대상이 되는 객체)
+        m.addAttribute("sayHello","Hello, World! from Controller");
+
         return "index";
     }
 }
