3eea7bcf800a25c1f751344397cd4252eac57c34
nou <nou13n8e@gmail.com>
2023-06-28 18:04:26 +0900

    회원 가입 처리 Service

---
+++ src/main/java/nou/hello/spring4/service/MemberServiceImpl.java
@@ -0,0 +1,19 @@
+package nou.hello.spring4.service;
+
+import nou.hello.spring4.dao.MemberDAO;
+import nou.hello.spring4.model.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("msrv")
+public class MemberServiceImpl implements MemberService {
+    @Autowired
+    private MemberDAO mdao;
+
+    @Override
+    public boolean saveMember(Member m) {
+        boolean isSaved=false;
+        if(mdao.insertMember(m) > 0) isSaved=true;
+        return isSaved;
+    }
+}
