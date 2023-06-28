33ab33cb53f2ed94c33ecc5cbbf103abfb52e7b4
nou <nou13n8e@gmail.com>
2023-06-28 18:03:57 +0900

    회원 가입 처리 Controller

--- src/main/java/nou/hello/spring4/controller/MemberController.java
+++ src/main/java/nou/hello/spring4/controller/MemberController.java
@@ -1,7 +1,10 @@
 package nou.hello.spring4.controller;
 
+import nou.hello.spring4.model.Member;
+import nou.hello.spring4.service.MemberService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MemberController {
 
     private Logger logger= LogManager.getLogger(MemberController.class);
+    @Autowired
+    private MemberService msrv;
 
     @RequestMapping(value = "/member/join", method = RequestMethod.GET)
     public String join(Model m) {
@@ -21,11 +26,14 @@ public class MemberController {
         return "member/join.tiles";
     }
     @RequestMapping(value = "/member/join", method = RequestMethod.POST)
-    public String joinok(Model m) {
-
+    public String joinok(Member m) {
         logger.info("member/joinok 호출!");
+        String viewName="/member/fail";
+        if(msrv.saveMember(m)){
+            viewName="redirect:/member/login";
+        };
 
-        return "redirect:/member/login";
+        return viewName;
     }
     @RequestMapping(value = "/member/login", method = RequestMethod.GET)
     public String login(Model m) {
