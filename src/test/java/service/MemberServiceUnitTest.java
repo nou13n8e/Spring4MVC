45b03386665c67dacfece66343f1801f557f2a92
nou <nou13n8e@gmail.com>
2023-06-29 14:44:06 +0900

    MemberService 단위 테스트 추가

---
+++ src/test/java/service/MemberServiceUnitTest.java
@@ -0,0 +1,42 @@
+package service;
+
+import nou.hello.spring4.controller.MemberController;
+import nou.hello.spring4.model.Member;
+import nou.hello.spring4.service.MemberService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/servlet-context.xml",
+                         "classpath:spring/root-context.xml"})
+@WebAppConfiguration
+public class MemberServiceUnitTest {
+    @Autowired MemberService msrv;
+
+    @Test
+    @Transactional
+    public void insertMember() throws Exception {
+        Member m=new Member(null, "abc123a", "987xyz", "abc123", "abc123@987xyz", null);
+        assertEquals(msrv.saveMember(m), true);
+    }
+
+    @Test
+    @Transactional
+    public void loginMember() throws Exception {
+        Member m=new Member(null, "abc123a", "987xyz", "abc123", "abc123@987xyz", null);
+        assertEquals(msrv.loginMember(m), true);
+    }
+}
