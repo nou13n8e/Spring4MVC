14138319bf586c4dc5038539f930761b1cc2c4ec
nou <nou13n8e@gmail.com>
2023-06-30 16:52:16 +0900

    BoardDAO selectBoard 구현

---
+++ src/main/java/nou/hello/spring4/dao/BoardDAO.java
@@ -0,0 +1,11 @@
+package nou.hello.spring4.dao;
+
+import nou.hello.spring4.model.Board;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
+
+public interface BoardDAO {
+
+    List<Board> selectBoard(int snum);
+}
