95f3ca05135d4b90dc49519d8f9613847e40eb9b
nou <nou13n8e@gmail.com>
2023-06-30 16:53:14 +0900

    BoardService readBoard 구현

---
+++ src/main/java/nou/hello/spring4/service/BoardServiceImpl.java
@@ -0,0 +1,18 @@
+package nou.hello.spring4.service;
+
+import nou.hello.spring4.dao.BoardDAO;
+import nou.hello.spring4.model.Board;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("bsrv")
+public class BoardServiceImpl implements BoardService {
+    @Autowired BoardDAO bdao;
+    @Override
+    public List<Board> readBoard(int cpg) {
+        int snum=(cpg-1)*15;
+        return bdao.selectBoard(snum);
+    }
+}
