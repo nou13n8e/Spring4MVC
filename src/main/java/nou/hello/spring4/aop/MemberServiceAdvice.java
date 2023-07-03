7512ebafe4206abb2cf07ac177d64d263cd7c2e5
nou <nou13n8e@gmail.com>
2023-07-03 16:59:36 +0900

    AspectJ join에 대해 포인트컷, 어드바이스 추가

--- src/main/java/nou/hello/spring4/aop/MemberServiceAdvice.java
+++ src/main/java/nou/hello/spring4/aop/MemberServiceAdvice.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpSession;
@@ -14,7 +15,13 @@ import javax.servlet.http.HttpSession;
 public class MemberServiceAdvice {
     private Logger logger= LogManager.getLogger(MemberServiceAdvice.class);
 
-    @Around("execution(* nou.hello.spring4.controller.MemberController.info(..))")
+    @Pointcut("execution(* nou.hello.spring4.controller.MemberController.info(..))")
+    public void infoPoint() {
+    }
+    @Pointcut("execution(* nou.hello.spring4.controller.MemberController.join(..))")
+    public void joinPoint() {
+    }
+    @Around("infoPoint()")
     public Object infoAOPProcess(ProceedingJoinPoint pjp) throws Throwable {
         logger.info("infoAOPProcess 호출!");
         HttpSession sess=null;
@@ -27,6 +34,23 @@ public class MemberServiceAdvice {
 
         // pointcut 대상 메서드 실행
         if(sess.getAttribute("member") == null) return "redirect:/member/login";
+
+        Object obj=pjp.proceed();
+        return obj;
+    }
+    @Around("joinPoint()")
+    public Object joinAOPProcess(ProceedingJoinPoint pjp) throws Throwable {
+        logger.info("joinAOPProcess 호출!");
+        HttpSession sess=null;
+
+        for(Object o : pjp.getArgs()) {
+            if(o instanceof HttpSession)
+                sess=(HttpSession) o;
+        }
+
+        // pointcut 대상 메서드 실행
+        // 로그인 하지 않았다면 join으로 이동, 로그인 했다면 info로 이동
+        if(sess.getAttribute("member") != null) return "redirect:/member/info";
 
         Object obj=pjp.proceed();
         return obj;
