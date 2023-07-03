c27a8c0188c7ee314196a05d04aa01881a76f19e
nou <nou13n8e@gmail.com>
2023-07-03 14:53:48 +0900

    BoardController : writeok 구현

--- src/main/java/nou/hello/spring4/controller/BoardController.java
+++ src/main/java/nou/hello/spring4/controller/BoardController.java
@@ -1,5 +1,7 @@
 package nou.hello.spring4.controller;
 
+import nou.hello.spring4.model.Board;
+import nou.hello.spring4.model.Member;
 import nou.hello.spring4.service.BoardService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -7,7 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/board")
 public class BoardController {
@@ -20,14 +27,21 @@ public class BoardController {
         m.addAttribute("boards", bsrv.readBoard(cpg));
 //        m.addAttribute("psnum", );  //페이지네이션 시작할 번호(1, 11, 21)
 //        m.addAttribute("allpg", );  //페이지 총합을 구하는 함수를 이용
-
         return "board/list.tiles";
     }
     @GetMapping("/write")
     public String write() {
         logger.info("board/write 호출!");
-
         return "board/write.tiles";
+    }
+    @PostMapping(value = "/write")
+    public String writeok(Board bd) {
+        logger.info("member/writeok 호출!");
+        String returnPage="redirect:/board/fail";
+        if(bsrv.saveBoard(bd)) {
+            returnPage = "redirect:/board/list?cpg=1";
+        };
+        return returnPage;
     }
     @GetMapping("/view")
     public String view(Model m, String bno) {
