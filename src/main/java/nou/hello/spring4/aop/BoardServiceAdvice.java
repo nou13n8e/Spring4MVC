ecdbdada4d0478d7e4684a9f7f14b176f6333fdb
nou <nou13n8e@gmail.com>
2023-07-04 09:39:26 +0900

    writeAOPProcess 위치 변경

---
+++ src/main/java/nou/hello/spring4/aop/BoardServiceAdvice.java
@@ -0,0 +1,38 @@
+package nou.hello.spring4.aop;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpSession;
+
+@Aspect
+@Component
+public class BoardServiceAdvice {
+    private Logger logger= LogManager.getLogger(MemberServiceAdvice.class);
+
+    @Pointcut("execution(* nou.hello.spring4.controller.BoardController.write(..))")
+    public void writePoint(){
+    }
+    @Around("writePoint()")
+    public Object writeAOPProcess(ProceedingJoinPoint pjp) throws Throwable {
+        logger.info("writeAOPProcess 호출!");
+        HttpSession sess=null;
+
+        for(Object o : pjp.getArgs()) {
+            if(o instanceof HttpSession)
+                sess=(HttpSession) o;
+        }
+
+        // pointcut 대상 메서드 실행
+        // 로그인 하지 않았다면 write으로 이동, 로그인 했다면 login로 이동
+        if(sess.getAttribute("member") == null) return "redirect:/member/login";
+
+        Object obj=pjp.proceed();
+        return obj;
+    }
+}
